2b2689b0ee9971623fd411ea5eaf7d4f1e4610d7
kris <grofernioken@users.noreply.github.com>
2016-11-07 14:31:37 -0800

    updated github repo

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.student.courtcounter;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewVariable;
+    public int score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewVariable = (TextView)findViewById(R.id.textViewName);
+        textViewVariable.setText("0");
+        score = 0;
+        textViewVariable.setText( String.valueOf(score) );
+    }
+    public void clickFunction1(View view){
+        score++;
     }
 }
